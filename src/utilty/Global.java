a10b5a2579069df3deac792876f2daf6e675a6e8
ahmadkisswani <67906076+ahmadkisswani@users.noreply.github.com>
2020-12-24 13:25:23 -0800

    Add files via upload

---
+++ src/utilty/Global.java
@@ -0,0 +1,14 @@
+package utilty;
+
+/**
+ *
+ * @author Ahmad kisswani
+ */
+public class Global {
+
+    public final static int minArgSize = 23;
+    public final static Object nullVal = null;
+    public final static int null_int_Val = 0;
+    public final static double null_double_Val = 0.0;
+
+}
---
+++ src/youtub/dl/cmd/YoutubDlCmd.java
@@ -0,0 +1,21 @@
+package youtub.dl.cmd;
+
+/**
+ *
+ * @author Ahmad kisswani
+ */
+import utilty.Global;
+
+public class YoutubDlCmd {
+
+    public static void main(String[] args) {
+
+        if (args[0] != Global.nullVal && args[0].length() > Global.minArgSize) {
+
+        } else {
+
+        }
+
+    }
+
+}
