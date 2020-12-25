fd3f0bd1f5356bc9a502d9ba36f8b0ff470f0058
ahmadkisswani <ahmad17a21@gmail.com>
2020-12-25 10:03:04 -0800

    test

--- src/youtub/dl/cmd/YoutubDlCmd.java
+++ src/youtub/dl/cmd/YoutubDlCmd.java
@@ -5,6 +5,7 @@ package youtub.dl.cmd;
  * @author Ahmad kisswani
  */
 // 0.1 v 24/12/2020
+
 import utilty.Global;
 
 public class YoutubDlCmd {
@@ -12,11 +13,15 @@ public class YoutubDlCmd {
     public static void main(String[] args) {
 
         if (args[0] != Global.nullVal && args[0].length() > Global.minArgSize) {
-           
+          
+            
+         
+
         } else {
 
         }
 
     }
+
 
 }
